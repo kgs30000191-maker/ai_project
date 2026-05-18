b8ff782d3e9bf1b416f30e7c356da6c479843a10
kgs30000191-maker <kgs30000191@gmail.com>
2026-05-18 10:03:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 st.title(' 나의 첫 웹 서비스 만들기')
-st.text_input('이름을 입력해 주세요!')
-st.selectbox('좋아하는 음식을 선택하세요!',['불닭','쌀국수','떡볶이'])
-st.button('인사말 생성')
+a=st.text_input('이름을 입력해 주세요!')
+b=st.selectbox('좋아하는 음식을 선택하세요!',['불닭','쌀국수','떡볶이'])
+if st.button('인사말 생성'):
+  st.write(a+'님, 안녕하세요')
+  
